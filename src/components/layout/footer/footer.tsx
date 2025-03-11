9256406f479fd3816df7c9ee35f13d1a9aca4b3a
NikKowPHP <nik.kow@outlook.com>
2025-03-11 15:40:52 +0100

    finish footer

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,19 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
 import { footerConfig } from "@/config/footer";
-import { X, Instagram } from "lucide-react";
+import {  Instagram } from "lucide-react";
+
+const XIcon = ({ className }: { className: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      focusable="false"
+      style={{
+        userSelect: 'none',
+        display: 'inline-block',
+        flexShrink: 0,
+        color: 'white',
+      }}
+      className={className}
+    >
+      <g>
+        <path
+          fill="currentColor"
+          d="M214.75,211.71l-62.6-98.38,61.77-67.95a8,8,0,0,0-11.84-10.76L143.24,99.34,102.75,35.71A8,8,0,0,0,96,32H48a8,8,0,0,0-6.75,12.3l62.6,98.37-61.77,68a8,8,0,1,0,11.84,10.76l58.84-64.72,40.49,63.63A8,8,0,0,0,160,224h48a8,8,0,0,0,6.75-12.29ZM164.39,208,62.57,48h29L193.43,208Z"
+        />
+      </g>
+    </svg>
+  );
+}
 
 export function Footer() {
 
   const socialIcons = {
-    X: X,
+    X: XIcon,
     Instagram: Instagram,
   }
+
+ 
 
   return (
     <>
       <footer
-        className="bg-[#014441] w-full py-12 px-[20px] md:px-20 text-white"
+        className="bg-[#014441] w-full py-12 px-[20px] md:px-20 text-white text-base"
         itemScope
         itemType="https://schema.org/Organization"
       >
@@ -21,16 +47,16 @@ export function Footer() {
           <div className="flex flex-col justify-start lg:flex-row gap-[30px] sm:gap-[30px]  lg:gap-[100px]  w-full">
 
             {/* first row */}
-            <div>
-              <div className=" border-r border-gray-200 ">
+            <div className="flex gap-[20px]">
+              <div className=" ">
                 <Link
                   href="/"
-                  className="gap-2 self-start font-bold transition-all duration-300 max-w-[100px] max-h-[38px]"
+                  className="   max-w-[110px] max-h-[28px]"
                   title="Rose Medical"
                   aria-label="Rose Medical Homepage"
                 >
                   <Image
-                    src="/logo.svg"
+                    src="/logo-white.avif"
                     alt="Rose Medical Logo"
                     width={100}
                     height={100}
@@ -38,14 +64,15 @@ export function Footer() {
                   />
                 </Link>
               </div>
+              <span className="text-base border-r border-gray-200 h-[28px]"></span>
               <div className="text-base ">Disposable Endoscopic Accessories</div>
             </div>
 
 
             {/* second row*/}
-            <div>
+            <div className="flex justify-between gap-[20px]">
               {/* first column */}
-              <div>
+              <div className="flex flex-col gap-[10px]">
                 {footerConfig.aboutLinks.map((link) => (
                   <Link href={link.href} key={link.title}>{link.title}</Link>
                 ))}
@@ -53,7 +80,7 @@ export function Footer() {
               </div>
 
               {/* second column */}
-              <div>
+              <div className="flex flex-col gap-[10px]" >
                 {footerConfig.pageLinks.map((link) => (
                   <Link href={link.href} key={link.title}>{link.title}</Link>
                 ))}
@@ -70,7 +97,7 @@ export function Footer() {
             <p className="text-base">© {new Date().getFullYear()}. All rights reserved. Rose Medical. </p>
 
             {/* fifth row | social media */}
-            <div className="flex gap-4">
+            <div className="flex gap-4 items-center justify-end  ">
               <span>@rosemedical</span>
               {footerConfig.socialLinks.map((link) => {
                 // Get the proper icon component based on the icon property
