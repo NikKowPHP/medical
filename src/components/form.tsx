172c84303e80d087fe8672c86a4af79b93404655
NikKowPHP <nik.kow@outlook.com>
2025-03-13 13:15:22 +0100

    finish form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { ChevronDownIcon } from 'lucide-react';
 import { useState } from 'react';
 
 export const Form = () => {
@@ -57,58 +58,58 @@ export const Form = () => {
   };
 
   return (
-    <div className="bg-white rounded-lg shadow-md p-6 w-full max-w-md mx-auto">
+    <div className="bg-white rounded-3xl border border-gray-200 p-6 w-full max-w-md mx-auto">
       <div className="mb-4">
         <h5 className="text-lg font-semibold text-gray-800">
           <strong className="text-black">Write us</strong>
         </h5>
         <p className="text-gray-600">We'd love to hear from you!</p>
       </div>
-      <form className="space-y-4" onSubmit={handleSubmit}>
-        <div className="flex space-x-4">
-          <label className="block w-1/2">
-            <p className="text-gray-700 text-sm font-bold mb-2">First name</p>
+      <form className="space-y-6 pt-6 text-[16px]" onSubmit={handleSubmit}>
+          <label className="block ">
+            <p className="text-gray-700   mb-2">First name</p>
             <input
               type="text"
               required
               name="First"
               placeholder="Jane"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className=" appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 border-gray-400 leading-tight focus:outline-none focus:border-gray-300"
               value={formData.First}
               onChange={handleChange}
             />
           </label>
-          <label className="block w-1/2">
-            <p className="text-gray-700 text-sm font-bold mb-2">Last name</p>
+          <label className="block">
+            <p className="text-gray-700   mb-2">Last name</p>
             <input
               type="text"
               required
               name="Last"
               placeholder="Smith"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className=" appearance-none border rounded-lg  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-gray-300"
               value={formData.Last}
               onChange={handleChange}
             />
           </label>
-        </div>
         <label className="block">
-          <p className="text-gray-700 text-sm font-bold mb-2">Email</p>
+          <p className="text-gray-700  mb-2">Email</p>
           <input
             type="email"
             required
             name="Email"
             placeholder="jane@example.com"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className=" appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-gray-300"
             value={formData.Email}
             onChange={handleChange}
           />
         </label>
         <label className="block">
-          <p className="text-gray-700 text-sm font-bold mb-2">Location</p>
+          <p className="text-gray-700  mb-2">Location</p>
+          <div className="relative">
+
           <select
             name="Location"
             required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className=" appearance-none border bg-white rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-gray-300"
             value={formData.Location}
             onChange={handleChange}
           >
@@ -118,10 +119,12 @@ export const Form = () => {
             <option value="amsterdam">Amsterdam</option>
             <option value="barcelona">Barcelona</option>
           </select>
+            <ChevronDownIcon className="w-5 h-5 absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
+          </div>
         </label>
         <button
           type="submit"
-          className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="bg-gray-800 hover:bg-gray-900 rounded-lg text-white w-full py-2 px-4  focus:outline-none focus:border-gray-300"
         >
           Submit
         </button>
