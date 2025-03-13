2cca9039d37363ab338bc8d8729935d0c72c8685
NikKowPHP <nik.kow@outlook.com>
2025-03-13 09:58:19 +0100

    create contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import { ContactSection } from "@/helpers/componentsLoad";
+
+export default function ContactPage() {
+  return (
+    <div
+      className="relative overflow-hidden min-h-screen bg-white"
+      itemScope
+      itemType="https://schema.org/WebPage"
+    >
+      <ContactSection />
+    </div>
+  );
+}
