120a1f9c2aa5a5d267cfad207d79a786153b4343
NikKowPHP <nik.kow@outlook.com>
2025-03-14 09:43:24 +0100

    add logs for products

--- src/components/sections/product-section.server.tsx
+++ src/components/sections/product-section.server.tsx
@@ -2,8 +2,10 @@
 
 import { productService } from "@/lib/services/product.service";
 import { ProductSectionClient } from "./products-section.client";
+import logger from "@/lib/logger";
 
 export default async function ProductSectionServer() {
   const products = await productService.getProducts();
+  logger.log('products', products)
   return <ProductSectionClient products={products || []} />;
 }
