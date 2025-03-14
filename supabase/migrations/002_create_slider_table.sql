e9949e041ab02833b7911a3bd2237ad9f0d1cfdb
NikKowPHP <nik.kow@outlook.com>
2025-03-14 09:14:46 +0100

    update migration

--- supabase/migrations/002_create_slider_table.sql
+++ supabase/migrations/002_create_slider_table.sql
@@ -1,11 +1,11 @@
 -- Create the slider_items table (if it doesn't already exist)
-CREATE TABLE IF NOT EXISTS slider_items (
+CREATE TABLE IF NOT EXISTS medical_slider (
   id VARCHAR PRIMARY KEY,
   image_url VARCHAR NOT NULL
 );
 
 -- Insert seed data into slider_items table
-INSERT INTO slider_items (id, image_url)
+INSERT INTO medical_slider (id, image_url)
 VALUES 
   ('items.item1', '/product1.avif'),
   ('items.item2', '/alton.avif'),
